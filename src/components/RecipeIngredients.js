f002de213366039975b92b382b64e899106140f7
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-01 20:18:59 -0500

    update RecipeIngredients with rmwc

--- src/components/RecipeIngredients.js
+++ src/components/RecipeIngredients.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import IngredientInput from './IngredientInput';
 import IngredientList from './IngredientList';
+import { Card, CardPrimaryAction } from 'rmwc/Card';
+import { ListDivider } from 'rmwc/List';
+import './RecipeIngredients.css';
 
 const RecipeIngredients = ({
   ingredients = [],
   onIngredientInput = f => f
 }) => {
   return (
-    <div className="recipe-ingredients">
+    <Card outlined className="RecipeIngredients">
+      <CardPrimaryAction style={{ padding: '0rem 1rem' }}>
+        <h4>Ingredients</h4>
+      </CardPrimaryAction>
+      <ListDivider />
       <IngredientInput onIngredientInput={onIngredientInput} />
+      <ListDivider />
       <IngredientList ingredients={ingredients} />
-    </div>
+    </Card>
   );
 };
 
