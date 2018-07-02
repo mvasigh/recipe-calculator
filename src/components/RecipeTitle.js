94cddd12bfb134b74db262752ef35bac5a05296b
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-01 20:18:14 -0500

    update RecipeTitle component with material-components-web

--- src/components/RecipeTitle.js
+++ src/components/RecipeTitle.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TextField, { HelperText, Input } from '@material/react-text-field';
+import './RecipeTitle.css';
 
-const RecipeTitle = () => {
-  return <h1>Recipe Title</h1>;
-};
+class RecipeTitle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.title || '',
+      fullWidth: props.fullWidth || false
+    };
+  }
+
+  render() {
+    return (
+      <div className="RecipeTitle">
+        <TextField
+          className={`mdc-text-field RecipeTitle__field ${
+            this.state.fullWidth ? 'RecipeTitle--fullwidth' : ''
+          }`}
+          label=""
+        >
+          <Input
+            className="RecipeTitle__input"
+            placeholder="Recipe title..."
+            value={this.state.value}
+            onChange={e =>
+              this.setState({
+                value: e.target.value
+              })
+            }
+          />
+        </TextField>
+      </div>
+    );
+  }
+}
 
 export default RecipeTitle;
