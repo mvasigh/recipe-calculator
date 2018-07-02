f67892421c123ac689a1c7fcbe99198a1f8c2ead
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-01 20:20:23 -0500

    updae IngredientInput with rmwc and material-components-web

--- src/components/IngredientInput.js
+++ src/components/IngredientInput.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Typography } from 'rmwc/Typography';
+import IngredientInputField from './IngredientInputField';
+import { Select } from 'rmwc/Select';
+import Button from '@material/react-button';
+import './IngredientInput.css';
 
 // consider turning into a stateful component to make updating 'select' tag options with units from API easier
+
+// class IngredientInput extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   handleSubmit(e) {
+//     e.preventDefault();
+//   }
+
+//   render() {
+
+//   }
+// }
 
 const IngredientInput = ({ onIngredientInput = f => f }) => {
   let _name, _quantity, _unit;
@@ -14,23 +35,34 @@ const IngredientInput = ({ onIngredientInput = f => f }) => {
     onIngredientInput(ingredient);
   };
   return (
-    <form className="ingredient-input" onSubmit={handleSubmit}>
-      <div>Add ingredient:</div>
-      <input
-        type="text"
+    <form className="IngredientInput" onSubmit={handleSubmit}>
+      <Typography className="IngredientInput__heading" use="subtitle2">
+        Add ingredient:
+      </Typography>
+      <IngredientInputField
         ref={input => (_name = input)}
-        className="ingredient-input__name"
-      />
-      <input
+        label="Ingredient"
         type="text"
-        ref={input => (_quantity = input)}
-        className="ingredient-input__quantity"
+        fullWidth
       />
-      <select ref={select => (_unit = select)}>
+      <div className="IngredientInput__row">
+        <IngredientInputField
+          ref={input => (_quantity = input)}
+          label="Quantity"
+          type="text"
+        />
+        <Select
+          box
+          label="Unit"
+          ref={select => (_unit = select)}
+          options={['one', 'two', 'three']}
+        />
+        <Button>Add</Button>
+      </div>
+      {/* <select ref={select => (_unit = select)}>
         <option>Option 1</option>
         <option>Option 2</option>
-      </select>
-      <button>+</button>
+      </select> */}
     </form>
   );
 };
