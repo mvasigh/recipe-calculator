1a1681038b4dbb5f3bc8cc8093e69624601313b1
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-05 20:09:12 -0500

    remove rmwc import

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import IngredientListItem from './IngredientListItem';
-import { List } from 'rmwc/List';
 
 class IngredientList extends Component {
   render() {
     console.log(this.props.ingredients);
     return (
-      <List className="ingredient-list">
+      <div className="ingredient-list">
         {this.props.ingredients.map((ingredient, i) => (
           <IngredientListItem {...ingredient} key={i} />
         ))}
-      </List>
+      </div>
     );
   }
 }
